fff27382d567354d8574bed3de3455f46a21ef16
SlackSystem <slacksystem@users.noreply.github.com>
2023-07-13 14:43:12 -0400

    refactor(handlers.py): remove commented out code
    feat(handlers.py): uncomment image handling code to enable image processing
    The commented out code and unused imports were removed to clean up the code and improve readability. The image handling code was uncommented to enable the processing of images in messages. This feature allows the bot to handle screenshots, which can be useful for various purposes such as moderation or content sharing.

--- handlers.py
+++ handlers.py
@@ -1,5 +1,4 @@
 import logging
-# import sys
 from typing import List
 
 from discord import Attachment, Member, Message, TextChannel, Thread
@@ -8,20 +7,16 @@ from discord.errors import Forbidden, HTTPException, NotFound
 
 import logging_config
 
-# from discord.utils import _ColourFormatter
 
 logging_config.configure_logging()
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# handler = logging.StreamHandler(sys.stdout)
-# formatter = _ColourFormatter()
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
 
 
 async def handle_screenshots(message: Message):
     pictures: List[Attachment] = [
-        pic for pic in message.attachments if pic.content_type.startswith("image")
+        pic
+        for pic in message.attachments
+        if pic.content_type.startswith("image")  # type: ignore
     ]
     logger.debug(f"{len(pictures)=}")
     author: Member = message.author  # type: ignore
@@ -36,20 +31,20 @@ async def handle_screenshots(message: Message):
             name=f"Screenshot from {author.name}",
             message=message,
         )
-        # for index, picture in enumerate(pictures):
-        #     sender: Member | User = message.author
-        #     imagefile: File = await picture.to_file(filename="image", use_cached=True)
-        #     initial_description: str = (  # Description to prepend if first image
-        #         f"User {sender.name} posted {message.content} " if index == 1 else ""
-        #     )
-        #     # Create index'th embed
-        #     an_embed: Embed = Embed(
-        #         description=initial_description + f"{index}/{len(pictures)}",
-        #     )
-        #     # Add the image
-        #     an_embed.set_image(url="attachment://image")
+        for index, picture in enumerate(pictures):
+            sender: Member | User = message.author
+            imagefile: File = await picture.to_file(filename="image", use_cached=True)
+            initial_description: str = (  # Description to prepend if first image
+                f"User {sender.name} posted {message.content} " if index == 1 else ""
+            )
+            # Create index'th embed
+            an_embed: Embed = Embed(
+                description=initial_description + f"{index}/{len(pictures)}",
+            )
+            # Add the image
+            an_embed.set_image(url="attachment://image")
 
-        #     await new_thread.send(file=imagefile, embed=an_embed)
+            await new_thread.send(file=imagefile, embed=an_embed)
 
     else:
         if channel.permissions_for(author).manage_messages:
